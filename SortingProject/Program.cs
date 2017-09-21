f97cee7550f89890b2325c6b91499f7461d15e61
Wilson Huang <window012@hotmail.com>
2017-09-21 14:45:25 +0800

    Add project files.

---
+++ SortingProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(random.Next(1, 101));
+            }
+
+            Console.WriteLine("Before Sort:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+
+            Console.WriteLine();
+            List<int> result = SortingAlgorithm.BubbleSort(list);
+
+            Console.WriteLine("After Sort:");
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SortingProject/SortingAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingProject
+{
+    /// <summary>
+    /// 排序演算法
+    /// https://zh.wikipedia.org/wiki/%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95
+    /// </summary>
+    public static class SortingAlgorithm
+    {
+        public static List<int> BubbleSort(List<int> list)
+        {
+            int temp = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count - 1 - i; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
